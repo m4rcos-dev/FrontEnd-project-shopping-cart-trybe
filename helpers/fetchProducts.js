93e697cc26b966e84b042378a3789cc1a748aeb7
Marcos Paulo <dev.marcospereira@gmail.com>
2022-07-11 22:38:16 -0300

    Requisito 1

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,5 +1,12 @@
-const fetchProducts = () => {
-  // seu código aqui
+const fetchProducts = async (query) => {
+  const url = `https://api.mercadolibre.com/sites/MLB/search?q=${query}`;
+  try {
+    const responseUrl = await fetch(url);
+    const data = await responseUrl.json();
+    return data;
+  } catch (erro) {
+    throw new Error('You must provide an url');
+  }
 };
 
 if (typeof module !== 'undefined') {
