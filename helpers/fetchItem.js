3fcc50c9d26ce28ea61145598106b066a414e6cc
Marcos Paulo <dev.marcospereira@gmail.com>
2022-07-13 11:23:59 -0300

    Requisito 3

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,5 +1,12 @@
-const fetchItem = () => {
-  // seu código aqui
+const fetchItem = async (itemID) => {
+  const url = `https://api.mercadolibre.com/items/${itemID}`;
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (erro) {
+    throw new Error('You must provide an url');
+  }
 };
 
 if (typeof module !== 'undefined') {
