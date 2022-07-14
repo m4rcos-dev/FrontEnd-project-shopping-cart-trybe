99f8fb53d6b9b22ea3b9a9f57f6795557e98d611
Marcos Paulo <dev.marcospereira@gmail.com>
2022-07-14 20:20:44 -0300

    Requisitos 9 e 10

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+const round = (num, places) => {
+  if (!(`${num}`).includes('e')) {
+    return +(`${Math.round(`${num}e+${places}`)}e-${places}`);
+  }
+  const arr = (`${num}`).split('e');
+  let sig = '';
+  if (+arr[1] + places > 0) {
+    sig = '+';
+  }
+  return +(`${Math.round(`${+arr[0]}e${sig}${+arr[1] + places}`)}e-${places}`);
+};
+// Creditos da função acima ao A Kunin, user:1736537 do stackoverflow, apenas modificada do original para as regras do linter e funcionamento na minha aplicação. Função utilizada para trabalhar com casas decimais de forma mais precisa possível.
+
 const createProductImageElement = (imageSource) => {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -26,16 +39,41 @@ const createProductItemElement = ({ sku, name, image }) => {
 
 const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
 
-const cartItemClickListener = (event) => {
+const selectTotalPrice = () => {
+  priceContainer = document.querySelector('.total-price');
+  return priceContainer;
+};
+
+const subtractTotalCart = (event) => {
+  const itemCartRemove = event.path[0];
+  const priceCapture = itemCartRemove.innerText.split(' ').pop().split('$').pop();
+  const priceConvertion = parseFloat(selectTotalPrice().innerText) - parseFloat(priceCapture);
+  // const priceTwoDecimals = priceConvertion.toFixed(2);
+  const priceRound = round(priceConvertion, 2);
+  selectTotalPrice().innerText = priceRound;
+  localStorage.setItem('total', priceRound);
+};
+const handleAllLocalStorage = (event) => {
+  localStorage.removeItem('cartItems');
+  subtractTotalCart(event);
+  localStorage.removeItem('total');
+};
+
+const removeItemCart = (event) => {
   const itemCartRemove = event.path[0];
   itemCartRemove.remove();
   if (getSavedCartItems('cartItems').split(',').length === 1) {
-    return localStorage.removeItem('cartItems');
+    return handleAllLocalStorage(event);
   }
   const idCapture = itemCartRemove.innerText.split(' ')[1];
   const ls = getSavedCartItems('cartItems').split(',');
   const newLs = ls.filter((element) => element !== idCapture);
   saveCartItems(newLs.toString());
+  subtractTotalCart(event);
+};
+
+const cartItemClickListener = (event) => {
+  removeItemCart(event);
 };
 
 const createCartItemElement = ({ sku, name, salePrice }) => {
@@ -74,16 +112,49 @@ const addItemCartHtml = async (itemID) => {
   cartContainer.appendChild(createCartItemElement(item));
 };
 
-const buttonItemAdd = document.querySelector('.container');
-buttonItemAdd.addEventListener('click', function (e) {
-  if (e.target.classList.contains('item__add')) {
-    const idItem = getSkuFromProductItem(e.target.parentNode);
-    addItemCartHtml(idItem);
+const addTotalCart = async (itemID) => {
+  const data = await fetchItem(itemID);
+  const { price } = data;
+  const priceConvertion = parseFloat(selectTotalPrice().innerText) + price;
+  const priceRound = round(priceConvertion, 2);
+  selectTotalPrice().innerText = priceRound;
+  localStorage.setItem('total', priceRound);
+};
+
+const loadTotalStorage = () => {
+  const priceContainer = document.querySelector('.total-price');
+  const currentStorage = () => {
+    priceContainer.innerText = localStorage.getItem('total');
+  };
+  if ('total' in localStorage) {
+    return currentStorage();
+  }
+  priceContainer.innerText = 0;
+};
+
+const createSumElement = () => {
+  const cartContainer = document.querySelector('.cart');
+  const elementSum = document.createElement('h3');
+  elementSum.className = 'total-price';
+  cartContainer.appendChild(elementSum);
+  loadTotalStorage();
+};
+
+const createItemCart = async (event) => {
+  if (event.target.classList.contains('item__add')) {
+    const idItem = getSkuFromProductItem(event.target.parentNode);
+    await addItemCartHtml(idItem);
+    await addTotalCart(idItem);
     if ('cartItems' in localStorage) {
       return saveCartItems(`${getSavedCartItems('cartItems')},${idItem}`);
     }
     saveCartItems(idItem);
   }
+};
+
+const buttonItemAdd = document.querySelector('.container');
+buttonItemAdd.addEventListener('click', function async(e) {
+  createItemCart(e);
 });
 
 const validSplit = () => {
@@ -104,7 +175,19 @@ const handleLocalStorage = () => {
   }
 };
 
-window.onload = () => {
-  createIntensHtml();
-  handleLocalStorage();
+const clearCart = () => {
+  if ('cartItems' in localStorage) {
+    localStorage.removeItem('cartItems');
+    localStorage.removeItem('total');
+    window.location.reload();
+  }
+};
+
+const buttonClear = document.querySelector('.empty-cart');
+buttonClear.addEventListener('click', () => clearCart());
+
+window.onload = async () => {
+  await createIntensHtml();
+  await handleLocalStorage();
+  await createSumElement();
 };
